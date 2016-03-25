cad9556a77d346596ae36999efc2fdcbd3601e69
Tony Bowden <tony@tmtm.com>
2016-03-25 12:48:21 +0000

    Use webhook instead of notifier

--- scraper.rb
+++ scraper.rb
@@ -5,4 +5,3 @@ require 'wikidata/fetcher'
 
 ids = EveryPolitician::Wikidata.wdq('claim[39:19853383]')
 EveryPolitician::Wikidata.scrape_wikidata(ids: ids, output: false)
-warn EveryPolitician::Wikidata.notify_rebuilder
